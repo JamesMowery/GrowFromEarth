d2573d0433adfc8b5ad4687cdb6b5011d88fd2a7
James Mowery <jmowery@gmail.com>
2015-12-18 18:39:36 -0500

    Fixes and optimizations
    I made a series of bug fixes, performance optimizations, screen
    resizing changes, and menu fixes.
    
    * I got rid of a dynamic physics effect for the stem (and the file
    associated with it), and replaced it with a static object. This should
    increase performance.
    * Added a tip to the menu to insist that the player users the keyboard
    keys
    * Locked the aspect ration to 4:3, and modified the world generation
    code slightly to be in line with the aspect ratio.
    * Got rid of an extra asset load, since the asset is no longer a part
    of the game
    * Enabled support for retina displays

--- src/controls.js
+++ src/controls.js
@@ -15,7 +15,7 @@ function playerControls() {
         plant.body.velocity.x = 650;
         plant.body.rotation = 35;
     }
-
+    
     // Move left mouse
     else if (game.input.mousePointer.isDown &&
              game.input.mousePointer.x < 400) {
@@ -38,17 +38,17 @@ function playerControls() {
 
     // Move right touch
     else if (game.input.pointer1.isDown && game.input.pointer1.x > 500) {
-        console.log("HOLDING");
         plant.body.velocity.x = 650;
         plant.body.rotation = 35;
     }
-
+    
     // Stop
     else {
         plant.body.velocity.x = 0;
         plant.body.rotation = 0;
     }
-
-    stem.x = plant.x - 3;
+    
+    // Ensures that the stem stays under the seed
+    stem.x = plant.x;
     
 }
--- src/world.js
+++ src/world.js
@@ -5,24 +5,27 @@ function createPlatforms() {
     // Create random platforms upward
     for (i = 0; i < 100; i++) {
         //Math.floor(Math.random()*(max-min+1)+min);
-        var rNum = Math.floor(Math.random()*(800-150+1)+150);
-        rNum = rNum - 1000;
-        platformLocationX = rNum;
+        var rNum = Math.floor(Math.random()*(600-0+1)+0);
+        
+        // Creates the left-most platform
         var platform = platforms.create(rNum,
             (-375 * i) - 800, 'platform');
-        platform.scale.setTo(2, 2);
+        platform.anchor.setTo(1, 0.5)
+        platform.scale.setTo(5, 2);
         platform.body.immovable = true;
         platform.visible = true;
 
-        platform = platforms.create(rNum + 1000, (-375 * i) - 800, 'platform');
-        platform.scale.setTo(2, 2);
+        // Creates the right-most platform
+        platform = platforms.create(rNum + 200, (-375 * i) - 800, 'platform');
+        platform.anchor.setTo(0, 0.5)
+        platform.scale.setTo(5, 2);
         platform.body.immovable = true;
         platform.visible = true;
-
-        platform = scorePlatforms.create(rNum + 500, (-375 * i) - 865, 'platform');
-        platform.scale.setTo(2, 2);
+        
+        // Creates an invisible platform to increase the score
+        platform = scorePlatforms.create(rNum - 100, (-375 * i) - 835, 'platform');
+        platform.scale.setTo(1, 0.1);
         platform.alpha = 0;
-        console.log("Create Platforms");
     }
 
     platformsExist = true;
@@ -31,12 +34,14 @@ function createPlatforms() {
 function createCollectables() {
 
     var i = 0;
-
+    
+    // Creates and places the sun tokens
+    
     for (i = 0; i < 100; i++) {
         //Math.floor(Math.random()*(max-min+1)+min);
         var rNum = Math.floor(Math.random()*(650-150+1)+150);
         var collectable = collectables.create(rNum,
-            (i * - 375) - 970, 'sun');
+            (i * - 375) - 1000, 'sun');
         collectable.scale.setTo(0.75);
         collectable.body.immovable = true;
         collectable.visible = true;
